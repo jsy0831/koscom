7b8ed7381faf5e245e45acf75c9381b47d65a9e6
ecourse <smkang@codenuri.co.kr>
2024-05-21 09:50:32 +0900

    Add files via upload

---
+++ DAY1/1_생성자2.cpp
@@ -0,0 +1,26 @@
+// 1_생성자2.cpp
+#include <string>
+#include <iostream>
+
+class Person
+{
+	std::string name;
+	int age;
+public:
+	Person(const std::string& n, int a) : name(n), age(a) {}
+};
+// Person 으로 부터 상속받는 Student 만들어 보세요
+// 1. int id  멤버 추가해 보세요
+// 2. 생성자 만들어 보세요
+// 3. main 에서 Student 객체 생성해 보세요
+
+
+
+
+int main()
+{
+//	Person p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
+	Person p2("kim", 20); // ok. 초기화된 객체. 안전하다!!
+}
+
+
