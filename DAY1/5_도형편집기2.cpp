1c683959485711412c536ef1df26affa53e392af
ecourse <smkang@codenuri.co.kr>
2024-05-21 14:11:24 +0900

    Add files via upload

--- DAY1/5_도형편집기2.cpp
+++ DAY1/5_도형편집기2.cpp
@@ -32,14 +32,16 @@ int main()
 		else if (cmd == 2) v.push_back(new Circle);
 		else if (cmd == 9)
 		{
-			for (auto p : v)
-				p->draw();
+			for (auto p : v) // A
+				p->draw();   // error. Shape 에는 draw 가 없다.
 		}
 	}
 }
 // 위 코드는 왜 에러일까요 ?
-// =>
+// => A 부분에서 p 의 타입은 Shape* 인데, Shape 에는 draw 가없다
 // 해결책을 생각해 보세요
+// 1. Shape* 인 p를 Rect* 나 Circle* 로 캐스팅해서 사용
+// 2. Shape 에도 draw() 를 만들자.
 
 
 
