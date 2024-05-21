6ba4a8c23c764adbbd7ba6167e2a6b14c50a49a5
ecourse <smkang@codenuri.co.kr>
2024-05-21 09:55:39 +0900

    Add files via upload

--- DAY1/1_생성자2.cpp
+++ DAY1/1_생성자2.cpp
@@ -14,11 +14,21 @@ public:
 // 2. 생성자 만들어 보세요
 // 3. main 에서 Student 객체 생성해 보세요
 
+class Student : public Person
+{
+	int id;
+public:
+//	Student(int n) : id(n) {} // error.
+//	Student(int n) : Person(), id(n) {} // 컴파일러가 변경한 코드
 
-
+	Student(const std::string& name, int age, int id)
+		: Person(name, age), id(id) {}
+};
 
 int main()
 {
+	Student s("kim", 20, 15);
+
 //	Person p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
 	Person p2("kim", 20); // ok. 초기화된 객체. 안전하다!!
 }
