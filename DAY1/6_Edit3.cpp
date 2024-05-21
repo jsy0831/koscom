c51680d247b0e1b35ccb64db8a63ad02749b780b
ecourse <smkang@codenuri.co.kr>
2024-05-21 16:11:22 +0900

    Add files via upload

--- DAY1/6_Edit3.cpp
+++ DAY1/6_Edit3.cpp
@@ -8,6 +8,20 @@
 // 2. 변하는 것을 다른 클래스로 분리 - Edit3.cpp
 
 
+// Validation 정책을 담은 클래스를 교체 가능하게 하려면
+// 인터페이스가 필요 하다.
+struct IValidator
+{
+	virtual bool validate(const std::string& s, char c) = 0;
+	virtual bool is_complete(const std::string& s) { return true; }
+	virtual ~IValidator() {}
+};
+
+// 주민등록번호 : 991  1    확인버튼
+
+
+
+
 class Edit
 {
 	std::string data;
