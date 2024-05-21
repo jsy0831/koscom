758c9cccf8f3a2cae8a60a9d655d8f63ace73f53
ecourse <smkang@codenuri.co.kr>
2024-05-21 16:47:29 +0900

    Add files via upload

--- DAY1/6_Edit4.cpp
+++ DAY1/6_Edit4.cpp
@@ -12,6 +12,8 @@
 
 // 특징 3. 정책 함수가 멤버 함수로 구현되므로 private 멤버에 접근 가능
 
+
+
 // #2. strategy
 // 특징 1. Edit 와 Validation 정책이 분리.. 
 //        다른 클래스도 LimitDigitValidator 사용가능
@@ -27,3 +29,12 @@
 // 1. Rect 를 그리는 방법은 Rect 만 알면 되고
 // 2. Rect 를 그리는 방법은 실행시간에 교체될 필요도 없다.
 // strategy 보다 template method 가 좋다!!! - private 접근 가능.
+
+
+// Edit1.cpp   : Validation 정책 교체 불가능 합니다. 가상함수를 사용하지 않고 직접조사. 
+//													빠릅니다.
+// Edit2/3.cpp : Validation 정책 교체 가능 합니다.   가상함수 호출이 있으므로 느립니다.
+
+// Validation 정책을 교체 할수 있는데 "성능저하 전혀없이 아주 빠르게" 할수 없을까요??
+// => 단위전략 디자인(내일 1교시)
+// => C++ 표준라이브러리인 STL이 사용하는 기술
