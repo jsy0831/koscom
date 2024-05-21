35ff511bd22e7c9ed4704091433c95b56084e484
ecourse <smkang@codenuri.co.kr>
2024-05-21 10:03:54 +0900

    Add files via upload

---
+++ DAY1/1_생성자3.cpp
@@ -0,0 +1,25 @@
+// protected 생성자의 의도 
+// => 자신은 객체를 만들수 없지만(추상적 존재. abstract)
+// => 파생 클래스는 객체를 만들수 있다(구체적 존재. concret)
+
+class Animal
+{
+//public:		// 1, 2 모두 에러 아님
+//private:		// 1, 2 모두 에러!
+protected:
+	Animal() {}
+};
+class Dog : public Animal
+{
+public:
+	Dog() {} // Dog() : Animal() {}
+};
+int main()
+{
+	// 다음중 에러를 모두 골라 보세요
+	Animal a;	// 1	error
+	Dog    d;	// 2	ok
+}
+
+
+
