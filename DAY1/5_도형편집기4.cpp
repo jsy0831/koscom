06639c6e3475ca92622d5caae49f7bad0e717f25
ecourse <smkang@codenuri.co.kr>
2024-05-21 14:33:45 +0900

    Add files via upload

--- DAY1/5_도형편집기4.cpp
+++ DAY1/5_도형편집기4.cpp
@@ -10,11 +10,23 @@
 
 class Shape
 {
+	int color;
 public:
 	virtual ~Shape() {}
 
+	// 아래 함수는 가상으로 할까요 ? 비 가상으로 할까요 ?
+	// => 파생 클래스가 override 할 필요 없다. - 비가상!!
+	void set_color(int c) { color = c; }
+
+	// 면적을 구하는 방법은 도형마다 다르다 
+	// => virtual 이어야 한다.
+	virtual int get_area() { return 0; }
+
 	virtual void draw() { std::cout << "draw Shape\n"; }
 };
+
+
+
 
 class Rect : public Shape
 {
@@ -38,10 +50,18 @@ int main()
 
 		if (cmd == 1) v.push_back(new Rect);
 		else if (cmd == 2) v.push_back(new Circle);
+
 		else if (cmd == 9)
 		{
 			for (auto p : v) 
-				p->draw();  
+				p->draw();  // 동일한 코드가 상황에 따라(객체의 종류)
+							// 다르게 동작
+							// 다형성(polymorphism)
+							// 객체지향 3대 특징 : 캡슐화, 상속, 다형성
+
+							// 다형성은 OCP 를 만족하는 좋은 코드 입니다
+							// "p->draw()" 는 새로운 도형이 추가되어도 
+							// 수정될 필요 없습니다.
 		}
 	}
 }
