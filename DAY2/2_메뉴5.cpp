998e12666e9fa31326b1e477ca8848db870e3d53
ecourse <smkang@codenuri.co.kr>
2024-05-22 11:05:14 +0900

    Add files via upload

--- DAY2/2_메뉴5.cpp
+++ DAY2/2_메뉴5.cpp
@@ -84,19 +84,28 @@ int main()
 
 	root->add(pm1);
 	root->add(pm2);
+//	pm1->add(pm2);
 
 	pm1->add(new MenuItem("HD",  11));
 	pm1->add(new MenuItem("FHD", 12));
 	pm1->add(new MenuItem("UHD", 13));
+	pm1->add(new MenuItem("8K", 14));
 
 	pm2->add(new MenuItem("RED",   21));
 	pm2->add(new MenuItem("GREEN", 22));
 	pm2->add(new MenuItem("BLUE",  23));
 
 	// 이제 시작하려면 ?
+	root->command();
 
 }
 
+// 객체지향 프로그래밍 에서 프로그램이란 ?
 
+// 1. 객체를 생성하고
+// 2. 객체간의 관계를 설정하고
+// 3. 객체간의 메세지를 주고 받는 과정이다(서로의 멤버함수 호출한다는 의미)
+
+// 4. 프로그램의 기본 단위는 "함수" 가 아닌 "클래스" 이다
 
 
