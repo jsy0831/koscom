0fb5776abe6ed1454be50cde97230006c46cbbf0
ecourse <smkang@codenuri.co.kr>
2024-05-22 13:17:24 +0900

    Add files via upload

---
+++ DAY2/3_menu_event2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <functional>
+using namespace std::placeholders;
+
+void f1(int a)               { printf("f1 : %d\n", a); }
+void f2(int a, int b)        { printf("f2 : %d, %d\n", a, b);}
+void f3(int a, int b, int c) { printf("f3 : %d, %d, %d\n", a, b, c); }
+void f4(int a, int b, int c, int d) { printf("f4 : %d, %d, %d, %d\n", a, b, c, d);}
+
+class Dialog
+{
+public:
+	void Init(int a, int b)
+	{
+		printf("Dialog Init : %d, %d\n", a, b);
+	}
+};
+
+int main()
+{
+	// C 언어의 함수 포인터는 인자의 갯수가 동일한 함수 주소만 담을수 있습니다.
+	void(*f)(int);
+	f = &f1; // ok
+	f = &f2; // error
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
