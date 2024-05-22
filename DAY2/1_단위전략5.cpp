c5acfd4461fbf566e186a84c0efb93ae46c733d4
ecourse <smkang@codenuri.co.kr>
2024-05-22 09:48:01 +0900

    Add files via upload

--- DAY2/1_단위전략5.cpp
+++ DAY2/1_단위전략5.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <vector>
 
+// 사용자 정의 allocator 를 만들때는 지켜야 하는 규칙이 있습니다.
 template<typename T> class MyAlloc
 {
+public:
+	// #1. 메모리 할당/해지를 책임지는 2개의 함수
+	inline T* allocate(std::size_t sz)
+	{
+		printf("MyAlloc allocate : %d\n", sz);
 
+		return static_cast<T*>(malloc(sizeof(T) * sz));
+	}
+	inline void deallocate(T* p, std::size_t sz) 
+	{ 
+		printf("MyAlloc deallocate : %d, %p\n", sz, p);
+		free(p); 
+	}
+
+	// #2. 아래 3개의 멤버가 관례적으로 필요 합니다
+	typedef T value_type;
+	MyAlloc() {} 
+	template<typename U> MyAlloc(const MyAlloc<U>&) {}
 };
+
+
+
+
 
 int main()
 {
