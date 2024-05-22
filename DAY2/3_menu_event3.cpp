9f0684e6af30461a1beadb542602a08037baf80a
ecourse <smkang@codenuri.co.kr>
2024-05-22 13:42:40 +0900

    Add files via upload

--- DAY2/3_menu_event3.cpp
+++ DAY2/3_menu_event3.cpp
@@ -35,15 +35,21 @@ public:
 void foo(void* p)        { std::cout << "foo : " << (int)p << std::endl; }
 void goo(void* p, int a) { std::cout << "goo : " << (int)p << std::endl; }
 
-
 int main()
 {
 	// IOS 개발에 널리 사용되는 "통보센터" 라는 클래스를 만들어 봅시다.
 	NotificationCenter nc;
 
+	// 현재 코드에서 통보센터에 등록되는 함수는 "void(void*)" 이어야 합니다
+
 	nc.addObserver("LOWBATTERY", &foo);
-	nc.addObserver("LOWBATTERY", &goo);
-	nc.addObserver("DISCONNECT_WIFI", &goo);
+	nc.addObserver("LOWBATTERY",      std::bind(&goo, _1, 11)); // goo(void*, 11)
+	nc.addObserver("DISCONNECT_WIFI", std::bind(&goo, _1, 12));
+
+	nc.addObserver("DISCONNECT_WIFI", 
+					[](void* p) { std::cout << "lambda\n"; });
+
+
 
 	// 배터리 모듈쪽에서 배터리가 부족해지면
 	nc.postNotificationWithName("LOWBATTERY", (void*)30);
