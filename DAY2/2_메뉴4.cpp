1406a1ca01fa4e872af0f406220e495ed5610031
ecourse <smkang@codenuri.co.kr>
2024-05-22 10:38:55 +0900

    Add files via upload

---
+++ DAY2/2_메뉴4.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+class MenuItem
+{
+	std::string title;
+	int id;
+public:
+	MenuItem(const std::string& title, int id) : title(title), id(id) {}
+
+	std::string get_title() const { return title; }
+
+	void command()
+	{
+		std::cout << get_title() << " 메뉴가 선택됨\n";
+
+		_getch();
+	}
+};
+
+class PopupMenu
+{
+	std::string title;
+	std::vector<MenuItem*> v;
+public:
+	PopupMenu(const std::string& title) : title(title) {}
+
+	void add(MenuItem* m) { v.push_back(m); }
+
+	void command()
+	{
+		while (1)
+		{
+			system("cls");
+
+			auto sz = v.size();
+
+			for (int i = 0; i < sz; i++)
+			{
+				std::cout << i + 1 << ". " << v[i]->get_title() << "\n";
+			}
+			std::cout << sz + 1 << ". 종료\n";
+
+			std::cout << "메뉴를 선택하세요 >> ";
+			int cmd;
+			std::cin >> cmd;
+
+
+			if (cmd == sz + 1) // 종료 메뉴
+				break;
+
+			if (cmd < 1 || cmd > sz + 1) // 잘못된 입력
+				continue;
+
+
+			// 선택된 메뉴 실행
+			v[cmd - 1]->command();
+		}
+
+
+	}
+};
+
+
+int main()
+{
+	MenuItem m1("참치 김밥",   11);
+	MenuItem m2("소고기 김밥", 12);
+	MenuItem m9("라면",        21);
+
+
+	PopupMenu kimbab("김밥류");
+	kimbab.add(&m1);
+	kimbab.add(&m2);
+
+		
+	PopupMenu pm("오늘의 점심 메뉴");
+	pm.add(&kimbab); // ?? 핵심. 잘 생각해 보세요
+	pm.add(&m9);
+
+	pm.command(); 
+}
+
+
+
+
