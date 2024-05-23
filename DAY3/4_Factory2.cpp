e1a64a82fb34f4dd26966a601185cbfe6acd2549
ecourse <smkang@codenuri.co.kr>
2024-05-23 13:42:06 +0900

    Add files via upload

--- DAY3/4_Factory2.cpp
+++ DAY3/4_Factory2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include "Helper.h"
 
 class Shape
@@ -21,8 +22,15 @@ public:
 };
 
 // Rect 를 만드는 2가지 방법
-// 1. Rect* p1 = new Rect;
-// 2. Rect* p2 = Rect::create();
+// 1. Rect* p1 = new Rect;			// 객체 생성시 클래스 이름이 필요 합니다.
+// 2. Rect* p2 = Rect::create();	// 객체 생성시 함수주소만 알면 됩니다.
+
+// #1. C++ 은 클래스 이름을 자료구조에 보관하거나 함수 인자로 사용할수 없습니다.
+// vector.push_back("Rect"); // 클래스 이름을 보관한 것이 아니라 문자열 "Rect" 보관
+						  // 문자열 "Rect" 로 Rect 객체를 만들수는 없습니다.
+
+// #2. 함수포인터는 자료구조에 보관 가능합니다
+// vector.push_back(&Rect::create); // 보관된 함수로 "Rect" 객체 생성가능.!
 
 
 
@@ -48,15 +56,31 @@ public:
 class ShapeFactory
 {
 	MAKE_SINGLETON(ShapeFactory)
+
+	using CREATOR = Shape* (*)();      // 함수 포인터 타입
+	std::map<int, CREATOR> create_map; // <도형번호, 생성함수> 의 쌍을 보관하는 map
+
 public:
+	void register_shape(int type, CREATOR f)
+	{
+		create_map[type] = f;
+	}
+
 	Shape* Create(int type)
 	{
 		Shape* p = nullptr;
-		if (type == 1)	p = new Rect;
-		else if (type == 2)	p = new Circle;
+		
+		CREATOR f = create_map[type];
+
+		if (f != nullptr)
+			p = f();		// map 에 등록된 생성함수로 도형 생성
+
 		return p;
 	}
 };
+
+
+
 
 
 int main()
@@ -64,6 +88,11 @@ int main()
 	std::vector<Shape*> v;
 
 	ShapeFactory& factory = ShapeFactory::get_instance();
+
+	// 공장을 사용하기 전에 공장에 제품(도형)을 등록해야 합니다.
+	factory.register_shape(1, &Rect::create);
+	factory.register_shape(2, &Circle::create);
+
 
 	while (1)
 	{
