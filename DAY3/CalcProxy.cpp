574061dbcf34ddc16d18718a62d3c34683969882
ecourse <smkang@codenuri.co.kr>
2024-05-23 16:22:55 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -2,33 +2,34 @@
 #define USING_GUI
 #include "cppmaster.h"
 #include "ICalc.h"
+#include <atomic>
 
 class Calc : public ICalc
 {
 	int server;
+
+	std::atomic<int> ref = 0; // 멀티스레드에 안전하게 증가/감소 하는 변수
 public:
+	
+	void AddRef() { ++ref; } // 멀티 스레드 환경에서도 안전하게 1증가
+
+	void Release()
+	{
+		if (--ref == 0)
+			delete this;
+	}
+
+
 	Calc() { server = ec_find_server("Calc"); }
+
+	~Calc() { std::cout << "~Calc()\n"; }
 
 	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
 
-// DLL 사용자(CLIENT) 는 DLL 안의 클래스 이름을
-// 알지도 못하고, 알 필요도 없고, 사용해서도 안됩니다.
-
-// Calc 의 객체를 생성하는 함수는 DLL 내부에서 약속된 이름의 함수로 제공해야 합니다.
-extern "C" __declspec(dllexport)	// windows DLL 에서 함수 만들때 필요
+extern "C" __declspec(dllexport)	
 ICalc* CreateProxy()
 {
 	return new Calc;
 }
-
-// 리눅스환경의 C++ 컴파일러이름 : "gcc, g++"
-// 윈도우 환경우 MS가제공하는 C++ 컴파일러 : "cl.exe"
-
-// CalcProxy.cpp 소스가 있는 곳으로 이동후
-
-// cl CalcProxy.cpp /LD  /link user32.lib gdi32.lib kernel32.lib  
-// 
-// => /LD : DLL 로 빌드하라는 옵션
-// => /link 이하는 IPC 기술 때문에 필요
--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -5,10 +5,17 @@
 
 struct ICalc
 {
+	// reference counting  기반으로 객체의 수명을 관리할때는
+	// 인터페이스 안에 참조계수 관련 함수선언을 같이 제공해야 합니다.
+	virtual void AddRef() = 0;
+	virtual void Release() = 0;
+
+
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
 	virtual ~ICalc() {}
 };
+
 
 ICalc* get_proxy()
 {
