89f00a2b81ce9f0432c86d48f36d03e567b88f66
ecourse <smkang@codenuri.co.kr>
2024-05-23 16:13:09 +0900

    Add files via upload

---
+++ DAY3/1_Proxy5.cpp
@@ -0,0 +1,20 @@
+#define USING_GUI
+#include "cppmaster.h"
+#include "ICalc.h"
+
+int main()
+{
+	ICalc* calc = get_proxy(); // 이순간 최종적으로는 
+							   // DLL 내부의 CreateProxy() 가 호출되고
+							   // "new Calc" 로 객체를 만들게 됩니다.
+							   // delete 가 필요 하지 않을까요 ??
+
+	int n1 = calc->Add(10, 20);
+	int n2 = calc->Sub(10, 20);
+
+	delete calc; // 이렇게 해도 될까요?
+}
+
+
+
+
