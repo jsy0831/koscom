168a422e99f82d6acbfa82092fec116dbe9960d8
ecourse <smkang@codenuri.co.kr>
2024-05-23 13:17:20 +0900

    Add files via upload

---
+++ DAY3/3_Flyweight3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+// SRP : Single Responsibility Principle
+// => 하나의 클래스는 하나의 책임만 
+
+class Image
+{
+	std::string image_url;
+
+	Image(const std::string& url) : image_url(url)
+	{
+		std::cout << url << " Downloading..." << std::endl;
+	}
+
+public:
+	void Draw() { std::cout << "Draw " << image_url << std::endl; }
+
+	// ImageFactory 에서는 Image 의 private 멤버에 접근이 가능하도록!!
+	friend class ImageFactory;
+};
+
+// Image 객체를 생성/공유 하는 기능을 제공하는 클래스
+class ImageFactory
+{
+	std::map<std::string, Image*> image_map;
+public:
+	Image* Create(const std::string& url)
+	{
+		Image* img = nullptr;
+
+		auto it = image_map.find(url);
+
+		if (it != image_map.end())
+		{
+			img = it->second;
+		}
+		else
+		{
+			img = new Image(url);
+			image_map[url] = img;
+		}
+		return img;
+	}
+};
+
+
+
+
+
+
+
+int main()
+{
+	ImageFactory factory;
+
+	Image* img1 = factory.Create("www.naver.com/a.png");
+	img1->Draw();
+
+	Image* img2 = factory.Create("www.naver.com/a.png");
+	img2->Draw();
+
+}
+
+
+
