d47cb8b8995d5a3203cd3a2121b194c50c0a0c8d
ecourse <smkang@codenuri.co.kr>
2024-05-23 13:47:55 +0900

    Add files via upload

---
+++ DAY3/4_Factory3.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include "Helper.h"
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class ShapeFactory
+{
+	MAKE_SINGLETON(ShapeFactory)
+
+		using CREATOR = Shape * (*)();
+	std::map<int, CREATOR> create_map;
+
+public:
+	void register_shape(int type, CREATOR f)
+	{
+		create_map[type] = f;
+	}
+
+	Shape* Create(int type)
+	{
+		Shape* p = nullptr;
+
+		CREATOR f = create_map[type];
+
+		if (f != nullptr)
+			p = f();
+
+		return p;
+	}
+};
+
+// 자동으로 공장에 제품을 등록해주는 클래스
+class AutoRegister
+{
+public:
+	AutoRegister(int type, Shape* (*f)())
+	{
+		ShapeFactory::get_instance().register_shape(type, f);
+	}
+};
+
+// 전역변수의 생성자는 언제 호출되는지 생각해 보세요 
+AutoRegister ar(1, &Rect::create); 
+
+
+
+
+
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Rect" << std::endl; }
+
+
+	static Shape* create() { return new Rect; }
+};
+
+
+
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Circle" << std::endl; }
+
+	static Shape* create() { return new Circle; }
+};
+
+
+
+
+
+
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	ShapeFactory& factory = ShapeFactory::get_instance();
+
+
+	factory.register_shape(1, &Rect::create);
+	factory.register_shape(2, &Circle::create);
+
+
+
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd > 0 && cmd < 8)
+		{
+			Shape* s = factory.Create(cmd);
+
+			if (s != nullptr)
+			{
+				v.push_back(s);
+			}
+		}
+
+		else if (cmd == 9)
+		{
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i]->draw();
+			}
+		}
+	}
+}
+
+
+
+
