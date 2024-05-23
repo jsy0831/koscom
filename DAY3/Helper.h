76217ac93b6410cb5d50bfb4212a74ff16cc3493
ecourse <smkang@codenuri.co.kr>
2024-05-23 10:25:42 +0900

    Add files via upload

---
+++ DAY3/Helper.h
@@ -0,0 +1,14 @@
+// Helper.h
+#pragma once
+
+#define MAKE_SINGLETON(classname)						\	
+private:												\
+	classname() {}										\
+	classname(const classname&) = delete;				\
+	classname& operator=(const classname&) = delete;	\
+public:													\
+	static classname& get_instance()					\
+	{													\
+		static classname instance;						\
+		return instance;								\
+	}
